fdfed34f42067c63206f0bdf75ce5742337ea3f2
Bolas <ltzuen@gmail.com>
2025-03-08 03:23:23 +0800

    feat: 新增 IconArrowDown 組件並更新 CSS 主題設置以包含動畫效果

---
+++ src/components/IconArrowDown/IconArrowDown.tsx
@@ -0,0 +1,18 @@
+const IconArrowDown = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 24 24"
+    role="presentation"
+    fill="none"
+    fillRule="nonzero"
+    stroke="currentColor"
+    strokeWidth="4"
+    strokeLinecap="round"
+    strokeLinejoin="round"
+  >
+    <line x1="12" y1="5" x2="12" y2="19"></line>
+    <polyline points="19 12 12 19 5 12"></polyline>
+  </svg>
+)
+
+export default IconArrowDown
